452cf570b09c2dd117cf6924f4f84cfc6f2ea4c2
khaoula ouifaya <khaoula.ouifaya@centoria.fr>
2025-01-23 01:20:26 +0100

    use class card instead of lists of strings

--- src/componants/MemoryGame.tsx
+++ src/componants/MemoryGame.tsx
@@ -1,49 +1,61 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from 'react';
+import  Card  from './models/Card';
 
 function MemoryGame() {
   const [countClick, setCountClick] = useState(0);
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false); // Indicateur si le jeu est en cours
 
-  const cardsList: String[] = [
-    "🌽", "🍒", "🍩", "😎", "🌼", "🍉", "🏠", "⭐", "🍎",
-    "🍒", "🌽", "🍉", "🌼", "🍩", "⭐", "🏠", "🍎", "😎",
+  const cardsList: Card[] = [
+  new Card(1, "🌽"),
+  new Card(2, "🍒"),
+  new Card(3, "🍩"),
+  new Card(4, "😎"),
+  new Card(5, "🌼"),
+  new Card(6, "🍉"),
+  new Card(7, "🏠"),
+  new Card(8, "⭐"),
+  new Card(9, "🍎"),
+  new Card(10, "🍒"),
+  new Card(11, "🌽"),
+  new Card(12, "🍉"),
+  new Card(13, "🌼"),
+  new Card(14, "🍩"),
+  new Card(15, "⭐"),
+  new Card(16, "🏠"),
+  new Card(17, "🍎"),
+  new Card(18, "😎"),
   ];
 
-  const [selectedCardsIndex,setSelectedCardsIndex] = useState<Number[]>([]);
-  const [selectedCardsContent,setSelectedCardsContent] = useState<String[]>([]);
+  const [selectedCards,setSelectedCards] = useState<Card[]>([]);
   const [result,setResult]=useState(0);
 
-
-  const clickCard = (index: number, cardContent: String) => {
+  const clickCard = (cardContent : Card) => {
     setCountClick(countClick + 1);
-    const count = selectedCardsContent.filter(item => item===cardContent).length;
+    const count = selectedCards.filter(item => item.content===cardContent.content).length;
     if(count <= 1 ){
-      setSelectedCardsIndex(prevState => [...prevState, index]);
-      setSelectedCardsContent(prevState => [...prevState, cardContent]);
-      selectedCardsContent.map(card => {
-        if(cardContent === card){
-            setResult(result+1);
+      setSelectedCards(prevState => [...prevState, cardContent]);
+      selectedCards.map(card => {
+        if(cardContent.content === card.content){
+          setResult(result+1);
         }else{
-          let uniques = [...new Set(selectedCardsIndex)];
           setTimeout(() => {
-              setSelectedCardsIndex([]);
-              setSelectedCardsContent([]);
-        }, 1000); 
+            setSelectedCards([]);
+      }, 1000);
         }
       });
     }
   };
 
-  const displayListCards = (cards: String[]) => {
-    return cards.map((card, index) => (
-      <div key={index} className="col mb-2">
+  const displayListCards = (cards :Card[]) => {
+    return cards.map(card => (
+      <div key={card.index} className="col mb-2">
         <div
           className="card large-card shadow-sm d-flex align-items-center justify-content-center"
-          onClick={() => clickCard(index, card)}
-          style={selectedCardsIndex.includes(index) ? {background: "white"} : {background: "black"}}>
-          {selectedCardsIndex.includes(index) && <span>{card}</span>}
+          onClick={() => clickCard(card)}
+          style={selectedCards.some(c => c.index === card.index) ? {background: "white"} : {background: "black"}}>
+          {selectedCards.some(c => c.index === card.index) && <span>{card.content}</span>}
         </div>
       </div>
     ));
